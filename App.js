ee3e08408b27461a9d1031e803d42031f9c11035
Murugan V <Murugan.V@mu-sigma.com>
2024-05-13 11:35:58 +0530

    Added Basic files

---
+++ App.js
@@ -0,0 +1,19 @@
+const heading=React.createElement("div",{id:"heading"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"h1tag"),
+        React.createElement("h2",{},"h2tag"),
+    ]),
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"h1tag"),
+        React.createElement("h2",{},"h2tag"),
+    ]),
+]);
+
+ 
+
+
+
+
+
+
+const root=ReactDOM.createRoot(document.getElementById("root")).render(heading)
